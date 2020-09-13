5d5dc7802c8d499f76e7d739c5b7967e9085af37
dread <dread@DESKTOP-BS1AKPM>
2020-09-12 19:17:16 -0700

    Add project files.

---
+++ Homework2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //initialize first and last name variables
+            String first_name = "mark";
+            String last_name = "aloka";
+            // (check to see in variable assigned correctly) System.out.println(first_name);
+            //System.out.println(last_name);
+
+            String fn_1st = first_name.Substring(0, 1);
+            String ln_1st = last_name.Substring(0, 1);
+            // check to see if it works System.out.println(fn_1st);
+            // check to see if it works System.out.println(ln_1st);
+            String pig_first = first_name.Substring(1, 1).ToUpper() + first_name.Substring(2) + fn_1st + "ay";
+            String pig_last = last_name.Substring(1, 1).ToUpper() + last_name.Substring(2) + ln_1st + "ay";
+            Console.WriteLine(pig_first);
+            Console.WriteLine(pig_last);
+
+        }
+
+
+    }
+}
---
+++ Part 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double Fahrenheit;
+            double Celsius;
+            Console.WriteLine("Enter the degrees Fahrenheit you would like to translate to Celsius: ");
+            Fahrenheit = Convert.ToInt32(Console.ReadLine());
+            Celsius = 5 * (Fahrenheit - 32) / 9;
+            Console.WriteLine(Fahrenheit + " degrees Fahrenheit = " + Math.Round(Celsius, 1) + " degrees Celsius");
+        }
+    }
+}
